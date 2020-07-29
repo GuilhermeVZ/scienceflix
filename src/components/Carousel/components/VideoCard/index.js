293608dd975d02b049b85ae98eb2ed4356e269f9
GuilhermeVZ <guilhermeziivo@gmail.com>
2020-07-29 18:22:00 -0300

    span com titulo 1.0

--- src/components/Carousel/components/VideoCard/index.js
+++ src/components/Carousel/components/VideoCard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VideoCardContainer } from './styles';
+import { VideoCardContainer, SpanTitle} from './styles';
 
 function getYouTubeId(youtubeURL) {
   return youtubeURL
@@ -19,7 +19,9 @@ function VideoCard({ videoTitle, videoURL, categoryColor }) {
       target="_blank"
       style={{ borderColor: categoryColor || 'red' }}
       title={videoTitle}
-    />
+    >
+      <SpanTitle>{videoTitle}</SpanTitle>
+    </VideoCardContainer>
   );
 }
 
--- src/components/Carousel/components/VideoCard/styles.js
+++ src/components/Carousel/components/VideoCard/styles.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components';
 
+export const SpanTitle = styled.span`
+  opacity: 0;
+  background-color: rgba(0,0,0,.6);
+  color: #ffffff;
+  text-shadow: 1px 1px 3px #000000;
+  width:100%;
+  height:100%;
+  transition: all 1s;
+  padding-top: 40%;
+  padding-left: 10%;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 export const VideoCardContainer = styled.a`
   border: 2px solid;
   border-radius: 4px;
   text-decoration: none;
+  padding: 0;
   overflow: hidden;
   cursor: pointer;
   color: white;
@@ -17,7 +33,6 @@ export const VideoCardContainer = styled.a`
   position: relative;
   display: flex;
   align-items: flex-end;
-  padding: 16px;
   transition: all 1s;
 
   &:hover,
@@ -25,7 +40,11 @@ export const VideoCardContainer = styled.a`
     margin: 0 50px;
 
     transform: scale(1.4);
+
+
+
   }
+
 
   &:not(:first-child) {
     margin-left: 20px;
