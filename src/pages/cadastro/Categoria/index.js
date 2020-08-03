2945dede24fc47fe1d4ad2a6774b725d82814db5
GuilhermeVZ <guilhermeziivo@gmail.com>
2020-08-03 16:20:02 -0300

    aula 4 finalizada

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,7 +32,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = window.location.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://science-flix.herokuapp.com/categorias'; 
       fetch(URL)
        .then(async (respostaDoServer) =>{
         if(respostaDoServer.ok) {
